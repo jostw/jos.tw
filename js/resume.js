8580b8f3b4ef003d23bbded7f3b8f0f3d3f2498d
jos <mail@jos.tw>
2015-08-26 11:12:28 +0800

    Add: contact in header

--- js/resume.js
+++ js/resume.js
@@ -14,3 +14,11 @@ import "../css/resume.css";
 
 import "modernizr";
 import "./vendor/plugins";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const $email = document.getElementsByClassName("js-email")[0];
+
+    $email.href = "mailto:mail@jos.tw";
+    $email.title = "mail@jos.tw";
+    $email.innerHTML = "mail@jos.tw";
+});
