90ac4f2d9f94523dec5b99cfc2b53871dba51d2a
jos <mail@jos.tw>
2015-08-27 15:24:48 +0800

    Add: contact form loading and complete

--- js/section/contact.js
+++ js/section/contact.js
@@ -15,13 +15,44 @@ const firebase = new Firebase("https://jostw.firebaseio.com");
 
 document.addEventListener("DOMContentLoaded", () => {
     const $contactForm = document.getElementsByClassName("js-contact-form")[0];
+    const $contactFormName = document.getElementsByClassName("js-contact-form-name")[0];
+    const $contactFormEmail = document.getElementsByClassName("js-contact-form-email")[0];
+    const $contactFormMessage = document.getElementsByClassName("js-contact-form-message")[0];
+    const $contactFormComplete = document.getElementsByClassName("js-contact-form-complete")[0];
 
     $contactForm.onsubmit = () => {
+        $contactForm.className += " form-loading";
+
+        $contactFormName.readOnly = true;
+        $contactFormEmail.readOnly = true;
+        $contactFormMessage.readOnly = true;
+
         firebase.child("messages").push({
-            name: document.getElementsByClassName("js-contact-form-name")[0].value,
-            email: document.getElementsByClassName("js-contact-form-email")[0].value,
-            message: document.getElementsByClassName("js-contact-form-message")[0].value
+            name: $contactFormName.value,
+            email: $contactFormEmail.value,
+            message: $contactFormMessage.value
+        }, () => {
+            setTimeout(() => {
+                $contactForm.className += " form-complete";
+            }, 500);
         });
+
+        return false;
+    };
+
+    $contactFormComplete.onclick = () => {
+        $contactForm.className = "form js-contact-form";
+
+        $contactFormName.value = "";
+        $contactFormName.readOnly = false;
+
+        $contactFormEmail.value = "";
+        $contactFormEmail.readOnly = false;
+
+        $contactFormMessage.value = "";
+        $contactFormMessage.readOnly = false;
+
+        $contactFormName.focus();
 
         return false;
     };
