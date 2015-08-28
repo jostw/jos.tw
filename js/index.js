d8876b4e24178c74547a46de178a749cc0da5b33
jos <mail@jos.tw>
2015-08-28 16:22:38 +0800

    Update: project logo image to sprites

--- js/index.js
+++ js/index.js
@@ -20,13 +20,9 @@ import "../img/jos-tw.png";
 import "../img/jos.png";
 import "../img/jos-front-end-developer.png";
 
-import "../img/muzik-air.png";
 import "../img/muzik-air-screen.png";
-import "../img/muzik-online.png";
 import "../img/muzik-online-screen.png";
-import "../img/muzik-study.png";
 import "../img/muzik-study-screen.png";
-import "../img/imusic.png";
 import "../img/imusic-screen.png";
 
 import "../img/sparkler.png";
