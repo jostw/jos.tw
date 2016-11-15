28d8f286bdec644502be71fe09c67ebaafe567e6
jos <mail@jos.tw>
2016-11-15 12:55:48 +0800

    Add Message component

---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Message.css';
+
+class Message extends Component {
+  render() {
+    return (
+      <span className="message">Hello World!</span>
+    );
+  }
+}
+
+export default Message;
---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Message from '../components/Message';
+
+class App extends Component {
+  render() {
+    return (
+      <Message />
+    );
+  }
+}
+
+export default App;
