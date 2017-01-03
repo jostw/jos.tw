90ca2a6fc262af3fce25e671588f283d30b8cd9a
jos <mail@jos.tw>
2017-01-03 16:00:08 +0800

    Add html-minifier

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -58,7 +58,7 @@ class Index extends Component {
     return (
       <html lang={ lang }>
         <head>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
 
           <link rel="shortcut icon" href={ favicon } />
--- template.js
+++ template.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import React from 'react';
 import { renderToString, renderToStaticMarkup } from 'react-dom/server';
+import { minify } from 'html-minifier';
 
 import metaReducer from './src/reducers/metaReducer';
 import Index from './src/containers/Index';
@@ -10,16 +11,21 @@ const indexPath = path.join(__dirname, 'public');
 const resumePath = path.join(__dirname, 'public/resume');
 
 const templateName = 'index.html';
+const htmlMinifierOptions = {
+  collapseWhitespace: true,
+  minifyJS: true
+};
+
 const { index, resume } = metaReducer();
 
-fs.writeFileSync(path.join(indexPath, templateName), renderToString(
+fs.writeFileSync(path.join(indexPath, templateName), minify(renderToString(
   <Index { ...index } />
-));
+), htmlMinifierOptions));
 
 if (!fs.existsSync(resumePath)) {
   fs.mkdirSync(resumePath);
 }
 
-fs.writeFileSync(path.join(resumePath, templateName), renderToStaticMarkup(
+fs.writeFileSync(path.join(resumePath, templateName), minify(renderToStaticMarkup(
   <Index { ...resume } />
-));
+), htmlMinifierOptions));
